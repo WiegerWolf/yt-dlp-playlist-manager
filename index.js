2e60c1c2f16618cf02ca92e5729cc8bdde801a5c
Your Name <you@example.com>
2023-12-27 20:22:10 +0000

    Refactor directory creation and reading
    This commit refactors the code to improve directory creation and reading functionality. It introduces a separate function for creating a directory and handles the case when the directory does not exist. The main function now calls the readDir function to retrieve and display the files in the specified directory.

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 import { promises as fs } from 'fs';
 
-const directoryPath = './data';
+const DIRECTORYPATH = './data';
 
-async function main() {
+async function createDirectory(directoryPath) {
     try {
-        const files = await fs.readdir(directoryPath);
-        console.log('Files in ./data directory:');
-        files.forEach((file) => {
-            console.log(file);
-        });
+        await fs.mkdir(directoryPath);
+        console.log(`Directory ${directoryPath} created.`);
     } catch (err) {
-        console.error('Error reading directory:', err);
+        console.error('Error creating directory:', err);
     }
 }
 
-main();
+async function readDir(directoryPath) {
+    let files;
+    try {
+        files = await fs.readdir(directoryPath);
+        console.log(`Files in directory ${directoryPath}:`);
+    } catch (err) {
+        // handle ENOENT error: file or directory does not exist
+        if (err.code === 'ENOENT') {
+            console.log(`Directory ${directoryPath} does not exist.`);
+            await createDirectory(directoryPath);
+            return readDir(directoryPath);
+        } else {
+            console.error(`Error reading directory ${directoryPath}:`, err);
+        }
+    }
+    return files;
+}
+
+async function main(directoryPath) {
+    const files = await readDir(directoryPath);
+    console.log(files);
+}
+
+
+main(DIRECTORYPATH)
+    .catch((err) => console.error('Error in main:', err));
